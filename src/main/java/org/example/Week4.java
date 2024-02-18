c5a0e8a98d52e92e117b09729d67a6e4a86411d1
elephant_seal <mikan.ha.orange123@gmail.com>
2024-02-18 23:31:12 +0900

    Week4_Stream課題提出

--- src/main/java/org/example/Week4.java
+++ src/main/java/org/example/Week4.java
@@ -1,1 +1,16 @@
-//初期セットアップ
+import java.util.List;
+
+public class Week4 {
+    public static void main(String[] args) {
+        List<String> names = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida");
+        List<String> sortedResult = names.stream().sorted().toList();
+
+        long count = names.stream().filter(v -> v.startsWith("y")).count();
+        boolean hasYamada = names.stream().anyMatch(name -> name.equals("yamada"));
+
+        System.out.println(sortedResult);
+        System.out.println(count);
+        System.out.println(hasYamada);
+    }
+
+}
